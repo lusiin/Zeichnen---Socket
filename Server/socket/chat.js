a16a129e71dabfb5f8afe9538dc6deba9d210607
Luis_Luethi <michaelluisluethi@gmail.com>
2022-03-05 11:53:26 +0100

    delete test

--- Server/socket/chat.js
+++
@@ -1,8 +0,0 @@
-// chat.js
-  module.exports = (io, socket) => {
-    //Future socket listeners will be here
-    socket.on('new user', (username) => {
-      console.log(`${username} has joined the chat! ✋`);
-      io.emit("new user", username);
-    })
-  }
